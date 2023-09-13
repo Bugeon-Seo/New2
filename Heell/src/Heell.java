41597216aba38f60599e23965352f4a1d7ac556e
Bugeon-Seo <142025334+Bugeon-Seo@users.noreply.github.com>
2023-09-13 12:11:22 +0900

    gwg
    wgwgwgwg

---
+++ Heell/src/Heell.java
@@ -0,0 +1,9 @@
+
+public class Heell {
+	public static void main(String[] args) {
+		System.out.println("hello, World! ");
+		System.out.print("hello, World! ");
+		System.out.println("Difference between println and print");
+	}
+
+}
